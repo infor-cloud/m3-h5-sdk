187437e4f4f84ba9df599ff86df6e24171572876
Andreas Hallberg <andreas.hallberg@infor.com>
2020-02-11 12:38:32 +0100

    refactor: ids schematic chaing

--- schematics/src/add-ids/index.ts
+++ schematics/src/add-ids/index.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { addModuleImportToRootModule, getProjectFromWorkspace, getProjectTargetOptions } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/config';
@@ -7,71 +7,89 @@ import { getLatestPackageVersion } from '../util/npm';
 
 
 export function addIDS(_options: any): Rule {
-   return async (tree: Tree, context: SchematicContext) => {
-      await addIDSPackage(tree, context);
-      addSohoComponentsModule(tree, context);
-      addScripts(tree, context);
-      addAssets(tree, context);
-      addStyles(tree, context);
-      installDependencies(tree, context);
+   return (tree: Tree, context: SchematicContext) => {
+      return chain([
+         addIDSPackage(),
+         addSohoComponentsModule(),
+         addScripts(),
+         addAssets(),
+         addStyles(),
+         installDependencies(),
+      ])(tree, context);
    };
 }
 
-async function addIDSPackage(tree: Tree, context: SchematicContext) {
-   const idsPackage = await getLatestPackageVersion('ids-enterprise-ng');
-   context.logger.info(`Add ${idsPackage.name}@${idsPackage.version}`);
-   addPackageJsonDependency(tree, {
-      name: idsPackage.name,
-      version: idsPackage.version,
-      type: NodeDependencyType.Default,
-      overwrite: false,
-   });
+function addIDSPackage(): Rule {
+   return async (tree: Tree, context: SchematicContext) => {
+      const idsPackage = await getLatestPackageVersion('ids-enterprise-ng');
+      context.logger.debug(`Add ${idsPackage.name}@${idsPackage.version}`);
+      addPackageJsonDependency(tree, {
+         name: idsPackage.name,
+         version: idsPackage.version,
+         type: NodeDependencyType.Default,
+         overwrite: false,
+      });
+   };
 }
 
-function installDependencies(_tree: Tree, context: SchematicContext) {
-   context.logger.info('Install dependencies');
-   context.addTask(new NodePackageInstallTask());
+function installDependencies(): Rule {
+   return (tree: Tree, context: SchematicContext) => {
+      context.logger.debug('Install dependencies');
+      context.addTask(new NodePackageInstallTask());
+      return tree;
+   };
 }
 
-function addSohoComponentsModule(tree: Tree, context: SchematicContext) {
-   const workspace = getWorkspace(tree);
-   const project = getProjectFromWorkspace(workspace);
-   addModuleImportToRootModule(tree, 'SohoComponentsModule', 'ids-enterprise-ng', project);
-   context.logger.info('Add SohoComponentsModule');
-   return tree;
+function addSohoComponentsModule(): Rule {
+   return (tree: Tree, context: SchematicContext) => {
+      const workspace = getWorkspace(tree);
+      const project = getProjectFromWorkspace(workspace);
+      addModuleImportToRootModule(tree, 'SohoComponentsModule', 'ids-enterprise-ng', project);
+      context.logger.debug('Add SohoComponentsModule');
+      return tree;
+   };
 }
 
-function addScripts(tree: Tree, context: SchematicContext) {
-   const scripts = [
-      './node_modules/jquery/dist/jquery.js',
-      './node_modules/d3/build/d3.js',
-      './node_modules/ids-enterprise/dist/js/sohoxi.js',
-   ];
-   addResources(tree, 'scripts', scripts);
-   context.logger.info('Add IDS scripts');
+function addScripts(): Rule {
+   return (tree: Tree, context: SchematicContext) => {
+      const scripts = [
+         './node_modules/jquery/dist/jquery.js',
+         './node_modules/d3/build/d3.js',
+         './node_modules/ids-enterprise/dist/js/sohoxi.js',
+      ];
+      addResources(tree, 'scripts', scripts);
+      context.logger.debug('Add IDS scripts');
+      return tree;
+   };
 }
 
-function addAssets(tree: Tree, context: SchematicContext) {
-   const assets = [
-      {
-         'glob': '**/*',
-         'input': './node_modules/ids-enterprise/dist/css',
-         'output': '/assets/ids-enterprise/css'
-      },
-      {
-         'glob': '**/*',
-         'input': './node_modules/ids-enterprise/dist/js/cultures',
-         'output': '/assets/ids-enterprise/js/cultures'
-      }
-   ];
-   addResources(tree, 'assets', assets);
-   context.logger.info('Add IDS assets');
+function addAssets(): Rule {
+   return (tree: Tree, context: SchematicContext) => {
+      const assets = [
+         {
+            'glob': '**/*',
+            'input': './node_modules/ids-enterprise/dist/css',
+            'output': '/assets/ids-enterprise/css'
+         },
+         {
+            'glob': '**/*',
+            'input': './node_modules/ids-enterprise/dist/js/cultures',
+            'output': '/assets/ids-enterprise/js/cultures'
+         }
+      ];
+      addResources(tree, 'assets', assets);
+      context.logger.debug('Add IDS assets');
+      return tree;
+   };
 }
 
-function addStyles(tree: Tree, context: SchematicContext) {
-   const styles = ['node_modules/ids-enterprise/dist/css/light-theme.css'];
-   addResources(tree, 'styles', styles);
-   context.logger.info('Add IDS styles');
+function addStyles(): Rule {
+   return (tree: Tree, context: SchematicContext) => {
+      const styles = ['node_modules/ids-enterprise/dist/css/light-theme.css'];
+      addResources(tree, 'styles', styles);
+      context.logger.debug('Add IDS styles');
+      return tree;
+   };
 }
 
 function addResources(tree: Tree, resourceType: 'scripts' | 'styles' | 'assets', resources: unknown[]) {
