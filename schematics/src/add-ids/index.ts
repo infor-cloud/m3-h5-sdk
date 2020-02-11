f6ebcb291f5fd8d822df43b34531474cd0415031
Andreas Hallberg <andreas.hallberg@infor.com>
2020-02-11 10:23:55 +0100

    schematics sandbox test script

--- schematics/src/add-ids/index.ts
+++ schematics/src/add-ids/index.ts
@@ -3,6 +3,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 export function addIDS(_options: any): Rule {
    return (tree: Tree, _context: SchematicContext) => {
+      _context.logger.info('Hello add-ids!');
       return tree;
    };
 }
