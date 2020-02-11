72089a919fde80a7a5359e03b8edfb1c74a0b867
Andreas Hallberg <andreas.hallberg@infor.com>
2020-02-11 10:45:31 +0100

    schematic install ids

--- schematics/src/add-ids/index.ts
+++ schematics/src/add-ids/index.ts
@@ -1,9 +1,28 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { addPackageJsonDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import { getLatestPackageVersion } from '../util/npm';
 
 
 export function addIDS(_options: any): Rule {
-   return (tree: Tree, _context: SchematicContext) => {
-      _context.logger.info('Hello add-ids!');
-      return tree;
+   return async (tree: Tree, context: SchematicContext) => {
+      await addIDSPackage(tree, context);
+      await installDependencies(tree, context);
    };
+}
+
+async function addIDSPackage(tree: Tree, context: SchematicContext) {
+   const idsPackage = await getLatestPackageVersion('ids-enterprise-ng');
+   context.logger.info(`Add ${idsPackage.name}@${idsPackage.version}`);
+   addPackageJsonDependency(tree, {
+      name: idsPackage.name,
+      version: idsPackage.version,
+      type: NodeDependencyType.Default,
+      overwrite: false,
+   });
+}
+
+async function installDependencies(_tree: Tree, context: SchematicContext) {
+   context.logger.info('Install dependencies');
+   context.addTask(new NodePackageInstallTask());
 }
