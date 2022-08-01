64abf2d518518119536c684d09a15a62b6435680
swuendri <sven.wuendrich@web.de>
2022-08-01 21:56:16 +0200

    Add test for ApplicationServiceCore.

---
+++ m3-odin/projects/infor-up/m3-odin/test/m3/application-service-core.spec.ts
@@ -0,0 +1,36 @@
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { ApplicationServiceCore, UserServiceCore } from '../../m3';
+import { IMessage } from '../../m3/types';
+
+describe('ApplicationServiceCore', () => {
+    let service: ApplicationServiceCore;
+    const message: IMessage = {
+        m3Command: 'launch',
+        m3Parameter: { link: 'foo' }
+    };
+
+    beforeEach(() => {
+        service = new ApplicationServiceCore();
+    });
+
+    it('should return if H5 environment', () => {
+        UserServiceCore.isH5Host = true;
+        expect(service.isH5()).toBe(true);
+    });
+
+    it('should launch m3 app', () => {
+        const spySendMessage = jest.spyOn(service as any, 'sendMessage').mockImplementation((message) => {
+            expect(message).toStrictEqual(message);
+        });
+        service.launch(message.m3Parameter.link);
+        expect(spySendMessage).toHaveBeenCalled();
+    });
+
+    it('should post message', () => {
+        const spyPostMessage = jest.spyOn(parent, 'postMessage').mockImplementation((message) => {
+            expect(message).toBe('{\"m3Command\":\"launch\",\"m3Parameter\":{\"link\":\"foo\"}}');
+         });
+        service['sendMessage'](message);
+        expect(spyPostMessage).toHaveBeenCalled();
+    });
+});
