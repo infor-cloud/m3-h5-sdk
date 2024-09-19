e0dcfc7590dcf2457b3848bb377e4f0a82ac7f6c
William Hernebrink <william.hernebrink@infor.com>
2024-09-19 11:57:43 +0200

    revert accidental change

--- cli/src/commands/new.ts
+++ cli/src/commands/new.ts
@@ -141,7 +141,7 @@ const npmInstall = (projectDir: string) => {
 };
 
 const newAngularProject = async (options: INewProjectOptions) => {
-   const ngOptions = ['--skip-install', '--strict=true', '--standalone=true'];
+   const ngOptions = ['--skip-install', '--strict=false', '--standalone=false'];
    if (!options.git) {
       ngOptions.push('--skip-git');
    }
